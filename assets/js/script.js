e7946ebae35c40cdca695909500200d16b1cbb40
Kate Mcguane <katemcguane@hotmail.com>
2024-07-29 09:51:10 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {  /* helper function */
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongAnswer() {  /* Incremented when user gets answer wrong */
+    
+}
+
+// Functions to display questions
+function displayAdditionQuestion() {
+    
+}
+
+function displaySubtractionQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
+function displayDivideQuestion() {
+    
+}
